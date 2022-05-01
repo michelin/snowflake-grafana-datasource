353926b2985272539b45f4d84ace5013eb5e78e6
devnied <mxjulien@gmail.com>
2022-05-01 23:10:08 +0200

    Fix unit tests

--- pkg/snowflake_test.go
+++ pkg/snowflake_test.go
@@ -50,17 +50,17 @@ func TestGetConnectionString(t *testing.T) {
 
 	t.Run("with User/pass", func(t *testing.T) {
 		connectionString := getConnectionString(&config, "password", "")
-		require.Equal(t, "username:password@account/database/schema?role=role&warehouse=warehouse&conf=xxx", connectionString)
+		require.Equal(t, "username:password@account?database=database&role=role&schema=schema&warehouse=warehouse&conf=xxx", connectionString)
 	})
 
 	t.Run("with private key", func(t *testing.T) {
 		connectionString := getConnectionString(&config, "", "privateKey")
-		require.Equal(t, "username@account/database/schema?authenticator=SNOWFLAKE_JWT&privateKey=privateKey&role=role&warehouse=warehouse&conf=xxx", connectionString)
+		require.Equal(t, "username@account?authenticator=SNOWFLAKE_JWT&database=database&privateKey=privateKey&role=role&schema=schema&warehouse=warehouse&conf=xxx", connectionString)
 	})
 
 	t.Run("with User/pass special char", func(t *testing.T) {
 		connectionString := getConnectionString(&config, "p@sswor/d", "")
-		require.Equal(t, "username:p%40sswor%2Fd@account/database/schema?role=role&warehouse=warehouse&conf=xxx", connectionString)
+		require.Equal(t, "username:p%40sswor%2Fd@account?database=database&role=role&schema=schema&warehouse=warehouse&conf=xxx", connectionString)
 	})
 
 	config = pluginConfig{
@@ -75,6 +75,6 @@ func TestGetConnectionString(t *testing.T) {
 
 	t.Run("with string to escape", func(t *testing.T) {
 		connectionString := getConnectionString(&config, "pa$$s&", "")
-		require.Equal(t, "user%40name:pa$$s&@acc@ount/dat@base/sch@ema?role=ro%40le&warehouse=ware%40house&conf=xxx", connectionString)
+		require.Equal(t, "user%40name:pa$$s&@acc@ount?database=dat%40base&role=ro%40le&schema=sch%40ema&warehouse=ware%40house&conf=xxx", connectionString)
 	})
 }
