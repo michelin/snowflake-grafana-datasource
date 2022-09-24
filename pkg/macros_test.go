ef27f4fdcf921096b79d7501d3d639a3666977f9
devnied <mxjulien@gmail.com>
2022-09-25 01:04:13 +0200

    Update macros

--- pkg/macros_test.go
+++ pkg/macros_test.go
@@ -36,7 +36,8 @@ func TestEvaluateMacro(t *testing.T) {
 		{name: "__timeEpoch", args: []string{"col"}, response: "extract(epoch from col) as time"},
 		// __timeFilter
 		{name: "__timeFilter", args: []string{}, err: "missing time column argument for macro __timeFilter"},
-		{name: "__timeFilter", args: []string{"col"}, config: configStruct, response: "col BETWEEN '" + timeRange.From.UTC().Format(time.RFC3339Nano) + "' AND '" + timeRange.To.UTC().Format(time.RFC3339Nano) + "'"},
+		{name: "__timeFilter", args: []string{"col"}, config: configStruct, response: "CONVERT_TIMEZONE('UTC', 'UTC', col) > '" + timeRange.From.UTC().Format(time.RFC3339Nano) + "' AND CONVERT_TIMEZONE('UTC', 'UTC', col) < '" + timeRange.To.UTC().Format(time.RFC3339Nano) + "'"},
+		{name: "__timeFilter", args: []string{"col", "'America/New_York'"}, config: configStruct, response: "CONVERT_TIMEZONE('UTC', 'America/New_York', col) > '" + timeRange.From.UTC().Format(time.RFC3339Nano) + "' AND CONVERT_TIMEZONE('UTC', 'America/New_York', col) < '" + timeRange.To.UTC().Format(time.RFC3339Nano) + "'"},
 		// __timeFrom
 		{name: "__timeFrom", args: []string{}, config: configStruct, response: "'" + timeRange.From.UTC().Format(time.RFC3339Nano) + "'"},
 		// __timeTo
@@ -44,17 +45,17 @@ func TestEvaluateMacro(t *testing.T) {
 		// __timeGroup
 		{name: "__timeGroup", args: []string{}, err: "macro __timeGroup needs time column and interval and optional fill value"},
 		{name: "__timeGroup", args: []string{"col", "xxxx"}, err: "error parsing interval xxxx"},
-		{name: "__timeGroup", args: []string{"col", "1d"}, response: "floor(extract(epoch from col)/86400)*86400"},
-		{name: "__timeGroup", args: []string{"col", "1d", "NULL"}, response: "floor(extract(epoch from col)/86400)*86400", fillMode: NullFill},
-		{name: "__timeGroup", args: []string{"col", "1d", "previous"}, response: "floor(extract(epoch from col)/86400)*86400", fillMode: PreviousFill},
-		{name: "__timeGroup", args: []string{"col", "1d", "12"}, response: "floor(extract(epoch from col)/86400)*86400", fillMode: ValueFill, fillValue: 12},
+		{name: "__timeGroup", args: []string{"col", "1d"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START')"},
+		{name: "__timeGroup", args: []string{"col", "1d", "NULL"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START')", fillMode: NullFill},
+		{name: "__timeGroup", args: []string{"col", "1d", "previous"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START')", fillMode: PreviousFill},
+		{name: "__timeGroup", args: []string{"col", "1d", "12"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START')", fillMode: ValueFill, fillValue: 12},
 		// __timeGroupAlias
 		{name: "__timeGroupAlias", args: []string{}, err: "macro __timeGroup needs time column and interval and optional fill value"},
 		{name: "__timeGroupAlias", args: []string{"col", "xxxx"}, err: "error parsing interval xxxx"},
-		{name: "__timeGroupAlias", args: []string{"col", "1d"}, response: "floor(extract(epoch from col)/86400)*86400 AS time"},
-		{name: "__timeGroupAlias", args: []string{"col", "1d", "NULL"}, response: "floor(extract(epoch from col)/86400)*86400 AS time", fillMode: NullFill},
-		{name: "__timeGroupAlias", args: []string{"col", "1d", "previous"}, response: "floor(extract(epoch from col)/86400)*86400 AS time", fillMode: PreviousFill},
-		{name: "__timeGroupAlias", args: []string{"col", "1d", "12"}, response: "floor(extract(epoch from col)/86400)*86400 AS time", fillMode: ValueFill, fillValue: 12},
+		{name: "__timeGroupAlias", args: []string{"col", "1d"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START') AS time"},
+		{name: "__timeGroupAlias", args: []string{"col", "1d", "NULL"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START') AS time", fillMode: NullFill},
+		{name: "__timeGroupAlias", args: []string{"col", "1d", "previous"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START') AS time", fillMode: PreviousFill},
+		{name: "__timeGroupAlias", args: []string{"col", "1d", "12"}, response: "TIME_SLICE(TO_TIMESTAMP(col), 86400, 'SECOND', 'START') AS time", fillMode: ValueFill, fillValue: 12},
 		{name: "__timeGroupAlias", args: []string{"col", "1d", "test"}, err: "error parsing fill value test"},
 		// __unixEpochFilter
 		{name: "__unixEpochFilter", args: []string{}, err: "missing time column argument for macro __unixEpochFilter"},
