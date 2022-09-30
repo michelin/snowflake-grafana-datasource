eeabcf780acf0b21c2c1c1833e1c4f7b867b3599
devnied <mxjulien@gmail.com>
2022-09-30 23:55:27 +0200

    Fix LIMIT condition to avoid duplicate LIMIT keyword. Add a default limit as the width of the panel.

--- pkg/query.go
+++ pkg/query.go
@@ -212,7 +212,7 @@ func (td *SnowflakeDatasource) query(dataQuery backend.DataQuery, config pluginC
 	}
 
 	// Add max Datapoint LIMIT option for time series
-	if queryConfig.MaxDataPoints > 0 && queryConfig.isTimeSeriesType() && strings.Contains(queryConfig.FinalQuery, "LIMIT ") {
+	if queryConfig.MaxDataPoints > 0 && queryConfig.isTimeSeriesType() && !strings.Contains(queryConfig.FinalQuery, "LIMIT ") {
 		queryConfig.FinalQuery = fmt.Sprintf("%s LIMIT %d", queryConfig.FinalQuery, queryConfig.MaxDataPoints)
 	}
 
