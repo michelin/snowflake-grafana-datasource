718dd483e27d781fe4ca9e97eccd280ec7979c60
Nikhil Benesch <nikhil.benesch@gmail.com>
2023-02-23 17:24:00 -0500

    Handle null bools, times, ints, and floats
    Fix #31.

--- pkg/query.go
+++ pkg/query.go
@@ -143,32 +143,30 @@ func (qc *queryConfigStruct) transformQueryResult(columnTypes []*sql.ColumnType,
 			continue
 		}
 
-		switch column_types[i].ScanType() {
-		case reflect.TypeOf(boolean):
-			values[i] = values[i].(bool)
-		case reflect.TypeOf(tim):
-			values[i] = values[i].(time.Time)
-		case reflect.TypeOf(integer):
-			n := new(big.Float)
-			n.SetString(values[i].(string))
-			precision, _, _ := columnTypes[i].DecimalSize()
-			if precision > 1 {
-				values[i], _ = n.Float64()
-			} else {
-				values[i], _ = n.Int64()
-			}
-		case reflect.TypeOf(float):
-			if v, err := strconv.ParseFloat(values[i].(string), 64); err == nil {
-				values[i] = v
-			} else {
-				log.DefaultLogger.Info("Rows", "Error converting string to float64", values[i])
-			}
-		case reflect.TypeOf(str):
-			if values[i] != nil {
+		if values[i] != nil {
+			switch column_types[i].ScanType() {
+			case reflect.TypeOf(boolean):
+				values[i] = values[i].(bool)
+			case reflect.TypeOf(tim):
+				values[i] = values[i].(time.Time)
+			case reflect.TypeOf(integer):
+				n := new(big.Float)
+				n.SetString(values[i].(string))
+				precision, _, _ := columnTypes[i].DecimalSize()
+				if precision > 1 {
+					values[i], _ = n.Float64()
+				} else {
+					values[i], _ = n.Int64()
+				}
+			case reflect.TypeOf(float):
+				if v, err := strconv.ParseFloat(values[i].(string), 64); err == nil {
+					values[i] = v
+				} else {
+					log.DefaultLogger.Info("Rows", "Error converting string to float64", values[i])
+				}
+			case reflect.TypeOf(str):
 				values[i] = values[i].(string)
-			}
-		default:
-			if values[i] != nil {
+			default:
 				values[i] = values[i].(string)
 			}
 		}
