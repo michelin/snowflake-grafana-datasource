57a13e351a8856af031d3488034345575b3c8839
devnied <mxjulien@gmail.com>
2023-03-06 17:46:06 +0100

    Fix unit test

--- pkg/gtime_test.go
+++ pkg/gtime_test.go
@@ -18,8 +18,8 @@ func TestParseInterval(t *testing.T) {
 		err      *regexp.Regexp
 	}{
 		{inp: "1d", duration: 24 * time.Hour},
-		{inp: "1w", duration: 168 * time.Hour},
-		{inp: "2w", duration: 2 * 168 * time.Hour},
+		{inp: "1w", duration: 24 * 7 * time.Hour},
+		{inp: "2w", duration: 24 * 7 * 2 * time.Hour},
 		{inp: "1M", duration: time.Duration(daysInMonth * 24 * int(time.Hour))},
 		{inp: "1y", duration: time.Duration(daysInYear * 24 * int(time.Hour))},
 		{inp: "5y", duration: time.Duration(calculateDays5y() * 24 * int(time.Hour))},
@@ -77,26 +77,15 @@ func calculateDays() (int, int) {
 	firstDayOfMonth := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, time.UTC)
 	daysInMonth := firstDayOfMonth.AddDate(0, 1, -1).Day()
 
-	t1 := time.Date(currentYear, 1, 1, 0, 0, 0, 0, time.UTC)
-	t2 := time.Date(currentYear+1, 1, 1, 0, 0, 0, 0, time.UTC)
-
-	daysInYear := int(t2.Sub(t1).Hours() / 24)
+	daysInYear := int(now.AddDate(1, 0, 0).Sub(now).Hours() / 24)
 
 	return daysInMonth, daysInYear
 }
 
 func calculateDays5y() int {
 	now := time.Now().UTC()
-	currentYear, _, _ := now.Date()
 
-	var daysInYear int
-
-	for i := 0; i < 5; i++ {
-		t1 := time.Date(currentYear+i, 1, 1, 0, 0, 0, 0, time.UTC)
-		t2 := time.Date(currentYear+i+1, 1, 1, 0, 0, 0, 0, time.UTC)
-
-		daysInYear = daysInYear + int(t2.Sub(t1).Hours()/24)
-	}
+	daysInYear := int(now.AddDate(5, 0, 0).Sub(now).Hours() / 24)
 
 	return daysInYear
 }
