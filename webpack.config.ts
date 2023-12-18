a0308a381e92a972acd194bd212917a1abff071d
devnied <mxjulien@gmail.com>
2023-12-19 00:51:07 +0100

    Add custom webpack config.

---
+++ webpack.config.ts
@@ -0,0 +1,36 @@
+import type { Configuration } from 'webpack';
+import grafanaConfig from './.config/webpack/webpack.config';
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import {hasReadme} from "./.config/webpack/utils";
+import {mergeWith} from "lodash";
+
+const config = async (env): Promise<Configuration> => {
+    const baseConfig = await grafanaConfig(env);
+
+    // merge with default congig, but replace plugins section
+    return mergeWith(baseConfig, {
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: hasReadme() ? 'README.md' : '../README.md', to: '.', force: true },
+                    { from: 'plugin.json', to: '.' },
+                    { from: '../LICENSE.txt', to: '.' },
+                    { from: '../CHANGELOG.md', to: '.', force: true },
+                    { from: '**/*.json', to: '.' },
+                    { from: '**/*.svg', to: '.', noErrorOnMissing: true }, // Optional
+                    { from: '**/*.png', to: '.', noErrorOnMissing: true }, // Optional
+                    { from: '**/*.html', to: '.', noErrorOnMissing: true }, // Optional
+                    { from: 'img/**/*', to: '.', noErrorOnMissing: true }, // Optional
+                    { from: 'libs/**/*', to: '.', noErrorOnMissing: true }, // Optional
+                    { from: 'static/**/*', to: '.', noErrorOnMissing: true }, // Optional
+                ],
+            }),
+        ],
+    }, (objValue, srcValue) => {
+        if (Array.isArray(objValue)) {
+            return srcValue;
+        }
+    });
+};
+
+export default config;
