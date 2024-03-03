dc722e7987c839c84a6a348fd1266c8afb1e06b9
Alex Nederlof <alex@nederlof.com>
2024-03-03 12:48:08 +0100

    Add Query cancellation
    Using the Grafana context, make sure queries can be cancelled when they are no longer needed. In Snowflake this can really clean up a lot of running queries.
    
    Also removes the unused HTTP Client

--- pkg/main.go
+++ pkg/main.go
@@ -8,9 +8,14 @@ import (
 )
 
 func main() {
-	// Start listening to requests send from Grafana. This call is blocking so
-	// it wont finish until Grafana shutsdown the process or the plugin choose
-	// to exit close down by itself
+	// Start listening to requests sent from Grafana. This call is blocking so
+	// it won't finish until Grafana shuts down the process or the plugin choose
+	// to exit by itself using os.Exit. Manage automatically manages life cycle
+	// of datasource instances. It accepts datasource instance factory as first
+	// argument. This factory will be automatically called on incoming request
+	// from Grafana to create different instances of SampleDatasource (per datasource
+	// ID). When datasource configuration changed Dispose method will be called and
+	// new datasource instance created using NewSampleDatasource factory.
 	err := datasource.Serve(newDatasource())
 
 	// Log any error if we could start the plugin.
